bff049e761a0b95a3d1d56852e550b626b7ff58f
mounikapujari <mounikapujari2005@gmail.com>
2024-12-02 15:07:33 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class typecasting{
+    public static void main(String args[]){
+        int a=50;
+        char b=(char)(a);
+        char c='5';
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
