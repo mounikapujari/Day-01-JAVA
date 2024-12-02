4d88b2f6c976502362c40da92007f1b61f273ab8
mounikapujari <mounikapujari2005@gmail.com>
2024-12-02 14:39:50 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Datatypes{
+    public static void main(String args[]){
+        int a=52;
+        char b='M';
+        float c=3.56f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
