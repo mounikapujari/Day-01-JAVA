4dcd285e2ffc54603495fa92bbd4a6cec792c51a
mounikapujari <mounikapujari2005@gmail.com>
2024-12-02 15:50:55 +0530

    Add files via upload

---
+++ acceptstring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class acceptstring{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        String a=S.next();
+        System.out.println("congratulations"+ a+"you won 5 lakhs");
+
+    }
+}
