7a6ec86a865d301aca07678fbee3d0514b455626
mounikapujari <mounikapujari2005@gmail.com>
2024-12-02 15:41:31 +0530

    Add files via upload

---
+++ Nextof.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Nextof{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=sc.next();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
